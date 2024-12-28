32a3ac4903b494670c947ec41bd17fbaa7f28b60
Zebulun McNeill <zeb.mcn@gmail.com>
2024-12-28 15:29:25 -0600

    render some basic html via the server

--- server/gitsoc.c
+++ server/gitsoc.c
@@ -3,9 +3,68 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <sys/stat.h>
 
 #define PORT 3000
 #define BUFFER_SIZE 1024
+#define HTML_DIR "./html"
+
+void get_file_path(char *path, char *full_path) {
+  if (strcmp(path, "/") == 0) {
+    snprintf(full_path, BUFFER_SIZE, "%s/index.html", HTML_DIR);
+  } else {
+    snprintf(full_path, BUFFER_SIZE, "%s%s.html", HTML_DIR, path);
+  }
+}
+
+void send_file(int client_socket, const char *file_path) {
+  FILE *file = fopen(file_path, "r");
+  if (!file) {
+    const char *custom_404_path = "./html/404.html";
+    FILE *custom_404 = fopen(custom_404_path, "r");
+    if (custom_404) {
+      const char *response_header =
+        "HTTP/1.1 404 Not Found\r\n"
+        "Content-Type: text/html\r\n"
+        "Connection: close\r\n\r\n";
+      send(client_socket, response_header, strlen(response_header), 0);
+
+      char file_buffer[BUFFER_SIZE];
+      size_t bytes_read;
+      while ((bytes_read = fread(file_buffer, 1, sizeof(file_buffer), custom_404)) > 0) {
+        send(client_socket, file_buffer, bytes_read, 0);
+      }
+      
+      fclose(custom_404);
+    } else {
+      const char *not_found_response =
+        "HTTP/1.1 404 Not Found\r\n"
+        "Content-Type: text/plain\r\n"
+        "Connection: close\r\n\r\n"
+        "404 Not Found";
+      send(client_socket, not_found_response, strlen(not_found_response), 0);
+    }
+
+    shutdown(client_socket, SHUT_WR);
+    close(client_socket);
+    return;
+  }
+
+  const char *response_header =
+    "HTTP/1.1 200 OK\r\n"
+    "Content-Type: text/html\r\n"
+    "Connection: close\r\n\r\n";
+  send(client_socket, response_header, strlen(response_header), 0);
+
+  char file_buffer[BUFFER_SIZE];
+  while (fgets(file_buffer, sizeof(file_buffer), file)) {
+    send(client_socket, file_buffer, strlen(file_buffer), 0);
+  }
+
+  fclose(file);
+  shutdown(client_socket, SHUT_WR);
+  close(client_socket);
+}
 
 void handle_client(int client_socket) {
   char buffer[BUFFER_SIZE];
@@ -24,13 +83,20 @@ void handle_client(int client_socket) {
   sscanf(buffer, "%s %s %s", method, path, version);
   printf("HTTP Method: %s\nPath: %s\nHTTP Version: %s\n", method, path, version);
 
-  const char *response =
-    "HTTP/1.1 200 OK\r\n"
-    "Content-Type: text/plain\r\n"
-    "Connection: close\r\n\r\n"
-    "Hello, World!";
-  send(client_socket, response, strlen(response), 0);
+  if (strcmp(method, "GET") != 0) {
+    const char *not_allowed_response =
+      "HTTP/1.1 405 Method Not Allowed\r\n"
+      "Content-Type: text/plain\r\n"
+      "Connection: close\r\n\r\n"
+      "405 Method Not Allowed";
+    send(client_socket, not_allowed_response, strlen(not_allowed_response), 0);
+    close(client_socket);
+    return;
+  }
 
+  char full_path[BUFFER_SIZE];
+  get_file_path(path, full_path);
+  send_file(client_socket, full_path);
   close(client_socket);
 }
 
